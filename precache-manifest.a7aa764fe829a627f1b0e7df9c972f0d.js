a181c33e7dafa535e8742fd103d398df42585a68
Marcin Olszowski <olszyna38@gmail.com>
2019-11-20 20:04:16 +0100

    Updates

---
+++ precache-manifest.a7aa764fe829a627f1b0e7df9c972f0d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4413bb4d73be5c0e28ddbd855a38909c",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "a8f6f420ec02b3a38ae1",
+    "url": "/todo_app/static/js/2.00d4a2de.chunk.js"
+  },
+  {
+    "revision": "9152a724985a8e90f4ec",
+    "url": "/todo_app/static/js/main.fcdcb014.chunk.js"
+  },
+  {
+    "revision": "b25f5c17a761d5227b67",
+    "url": "/todo_app/static/js/runtime-main.c7280b62.js"
+  }
+]);
