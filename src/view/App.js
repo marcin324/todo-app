3e4454078e0021a51eb9b2acdf79c94c71aef154
Marcin Olszowski <olszyna38@gmail.com>
2019-11-20 21:21:39 +0100

    Update App.js

--- src/view/App.js
+++ src/view/App.js
@@ -1,24 +1,21 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 import Todo from "./Todo";
 import Done from "./Done";
-import GlobalStyle from "../theme/GlobalStyle";
-import { theme } from "../theme/mainTheme";
+import MainTemplate from "../templates/MainTemplate";
 
 const App = () => {
   return (
     <div>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <MainTemplate>
           <Switch>
             <Route exact path="/" render={() => <Redirect to="/todo" />} />
             <Route path="/todo" component={Todo} />
             <Route path="/done" component={Done} />
           </Switch>
-        </BrowserRouter>
-      </ThemeProvider>
+        </MainTemplate>
+      </BrowserRouter>
     </div>
   );
 };
