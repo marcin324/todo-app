4d183ac57817a70d027e0f82923300180ed5771f
Marcin Olszowski <olszyna38@gmail.com>
2019-11-23 17:48:38 +0100

    Update file

--- src/view/App.js
+++ src/view/App.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import Todo from "./Todo";
 import Done from "./Done";
 import MainTemplate from "../templates/MainTemplate";
 
-const App = () => {
-  return (
-    <div>
-      <HashRouter basename="/" /* basename={process.env.PUBLIC_URL} */>
-        <MainTemplate>
-          <Switch>
-            <Route exact path="/" render={() => <Redirect to="/todo" />} />
-            <Route path="/todo" component={Todo} />
-            <Route path="/done" component={Done} />
-          </Switch>
-        </MainTemplate>
-      </HashRouter>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <HashRouter basename="/" /* basename={process.env.PUBLIC_URL} */>
+          <MainTemplate>
+            <Switch>
+              <Route exact path="/" render={() => <Redirect to="/todo" />} />
+              <Route path="/todo" component={Todo} />
+              <Route path="/done" component={Done} />
+            </Switch>
+          </MainTemplate>
+        </HashRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
