35fa5a7ecdc58692c9a625011cc4bf4b6dd181d1
Marcin Olszowski <olszyna38@gmail.com>
2019-11-24 21:53:57 +0100

    Update file

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,20 @@ import { applyMiddleware, createStore } from "redux";
 import { routerMiddleware } from "connected-react-router";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "../reducers/reducer";
-import { initialState } from "../reducers/taskReducer";
+// import { initialState } from "../reducers/taskReducer";
+
+export const initialState = {
+  todo: [
+    {
+      id: 0,
+      title: `Zdobyć pierwszą pracę jako frontend developer`,
+      deadline: `2020-01-01`,
+      important: true,
+      finishDate: null
+    }
+  ],
+  done: []
+};
 
 export default function configureStore({ history }) {
   const composeEnhancer = composeWithDevTools({});
